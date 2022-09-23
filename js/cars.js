a18ca91dbe0549de2d150564f2d2ade721dc1083
IsacHolstad <wisac22@gmail.com>
2022-09-23 19:14:21 +0200

    made 2 cars in array

---
+++ js/cars.js
@@ -0,0 +1,18 @@
+const cars = [
+    {
+        id: 1,
+        name: "Bmw",
+        wheelDrive: "Rwd",
+        color: "red",
+        new: true,
+        year: 2019,
+    },
+    {
+        id: 2,
+        name: "Mercedes",
+        wheelDrive: "Awd",
+        color: "silver",
+        new: false,
+        year: 2018,
+    },
+]
