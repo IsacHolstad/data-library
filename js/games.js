c6375b248d881f50d835fed86fe89899ca1c5e66
IsacHolstad <wisac22@gmail.com>
2022-09-26 14:46:48 +0200

    two ks files with more arrays

---
+++ js/games.js
@@ -0,0 +1,7 @@
+const games = [
+    { title: 'Mayhem Fighter', isMultiplayer: true, rating: 8 },
+    { title: 'Build-a-farm', isMultiplayer: true, rating: 9 },
+    { title: 'Ghost Story', isMultiplayer: false, rating: 8 },
+    { title: 'Fast Car Racer', isMultiplayer: true, rating: 7 },
+    { title: 'Elf and Dwarf RPG', isMultiplayer: false, rating: 8 },
+];
---
+++ js/people.js
@@ -0,0 +1,12 @@
+const people = [
+    { name: 'Kari', age: 28 },
+    { name: 'Astrid', age: 32 },
+    { name: 'Hans', age: 22 },
+    { name: 'Inger', age: 19 },
+    { name: 'Liv', age: 42 },
+    { name: 'Kristoffer', age: 12 },
+    { name: 'Anne', age: 12 },
+    { name: 'Martin', age: 17 },
+    { name: 'Joakim', age: 45 },
+    { name: 'Ellen', age: 7 },
+];
